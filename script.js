c2abd45e183e2ddfbb53b9fcbc0436826c40881f
Amos SOSSOUKPE <fenouamossossoukpe@gmail.com>
2024-01-24 12:29:42 +0100

    Version 2 du projet

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function telecharge(){
     var certificat = document.getElementById('attestation');
     
      var opt = {
-                margin: 10,
-                filename: `Attestation_Ambassadeur${Date.now()}.pdf`,
+                margin: 0,
+                filename: `Attestation_Ambassadeur${localStorage.getItem("nom")}.pdf`,
                 image: { type: 'jpeg', quality: 0.98 },
                 html2canvas: { scale: 2 },
-                jsPDF: { unit: 'mm', format: 'a4', orientation: 'landscape' } // Définir l'orientation sur 'landscape' pour le mode paysage
+                jsPDF: {unit: 'px', format: [800, 500], orientation: 'landscape'} // Définir l'orientation sur 'landscape' pour le mode paysage
             };
          html2pdf(certificat, opt);
 
@@ -22,7 +22,14 @@ function telecharge(){
           });
     
 }
+function capitalize(prenom) {
+    const partiesPrenom = prenom.split(" ");
 
+// Formate chaque partie du prenom
+const prenomFormate = partiesPrenom.map(prenom => `${prenom.charAt(0).toUpperCase()}${prenom.slice(1).toLowerCase()}`).join(" ");
+  
+ return prenomFormate;
+}
 
     const start = document.querySelector(".start");
     start.addEventListener("click", ()=>{
@@ -57,7 +64,7 @@ function telecharge(){
         const form_prenom = document.querySelector(".form_prenom").value;
         const form_date = document.querySelector(".form_date").value;
 
-        localStorage.setItem("key", `${form_prenom.toUpperCase()} ${form_nom.toUpperCase()}`)
+        localStorage.setItem("nom", form_prenom)
      
        
      
@@ -71,37 +78,35 @@ function telecharge(){
             </div>
             
             <div class="w-full h-screen flex  flex-col justify-center  bg-white " >
-                <div class="attestation w-[800px] h-[500px]  mx-auto relative  overflow-hidden" id="attestation">
+                <div class="attestation w-[800px] h-[500px] mx-auto relative  overflow-hidden" id="attestation">
                 
                 <img src="./assets/background.png" alt="" class="absolute top-0 left-0 right-0 bottom-0 ">
                 <img src="./assets/bordure.png" alt="" class="absolute top-0 left-0 right-0 bottom-0 w-[800px] h-[500px] ">
-                <div class=" w-full  absolute top-4">
-                    <div class="entete flex justify-evenly items-center">
-                        <img src="./assets/logo.png" alt="" class="w-20 h-20 object-cover mt-6">
-                        <h1 class="text-4xl font-bold font-cinzel mt-6 tracking-wider">ATTESTATION <br class="mt-4"> <span class="text-2xl">D'AMBASSADEUR</span></h1>
-                        <img src="./assets/logo.png" alt="" class="w-20 h-20 object-cover mt-5">
+                <div class="w-full  absolute top-4">
+                    <div class="entete flex justify-evenly items-center px-3">
+                        <img src="./assets/logo.png" alt="" class="w-32 h-32 object-cover mt-6 -ml-[70px] ">
+                        <h1 class="text-4xl font-bold font-cinzel mt-6 tracking-wider">ATTESTATION <br class="mt-4"> <span class="text-3xl text-center">D'AMBASSADEUR</span></h1>
+                        <img src="./assets/logo.png" alt="" class="w-32 h-32 object-cover mt-5 -mr-[70px]">
                     </div>
                     
                 
                     <div class="contenu flex flex-col justify-center items-center  ">
-                        <p class="text-xl mt-4">Décerné à :</p>
-                        <h2 class="text-2xl font-bold mt-5 font-glacial">${form_prenom.toUpperCase()}  ${form_nom.toUpperCase()}</h2>
-                        <hr class="h-8 w-[200px] border-slate-600 mt-2">
+                        <p class="text-xl mt-2">Décerné à :</p>
+                        <h2 class="text-2xl font-bold mt-3 font-glacial">${capitalize(form_prenom)} ${form_nom.toUpperCase()}</h2>
+                        <hr class="h-5 w-[200px] border-slate-600 mt-2">
                         <p class="text-xl">Est Ambassadeur de <span class="text-human font-bold">Human AI</span></p>
-                        <p class="text-xl">Depuis le <span>${form_date}</span></p>
+                        <p class="text-xl">Depuis le <span>${(new Date(form_date)).toLocaleDateString('fr-FR')}</span></p>
                     </div>
         
         
-                    <div class=" flex justify-center mt-3">
-                        <img src="./assets/bagde.png" alt="" class="w-32 h-32 object-cover">
-                        
-                        
+                    <div class="flex justify-center -mt-2">
+                        <img src="./assets/bagde.png" alt="" class="w-32 h-40 object-cover">                        
                     </div>
-                    <div class="relative -top-36">
-                        <img src="./assets/Cachet_Human_AI.png" alt="" class=" relative w-32 h-32 float-end mr-40 object-cover">
+                    <div class="relative -top-44 ">
+                        <img src="./assets/Cachet_Human_AI.png" alt="" class=" relative w-60 h-48 float-end mr-[134px] object-cover">
                         
                      </div>
-                     <img src="./assets/Signature_Human_AI.png" alt="" class="relative float-right  -right-48 -top-32 w-60 h-60  ">
+                     
                     
                 </div>
               
@@ -116,7 +121,8 @@ function telecharge(){
            </div>`
           
         }else{
-
+    
+        
             Swal.fire({
                 icon: "error",
                 title: "Oops...",
